efcdcda5e94f5e1c7b18e65fa757e44dd7028f84
Gökhan Gurbetoğlu <ggurbet@gmail.com>
2019-10-19 17:30:38 +0300

    Add Show Two Args example

---
+++ basics/ShowTwoArgs.java
@@ -0,0 +1,8 @@
+package basics;
+
+public class ShowTwoArgs {
+	public static void main(String[] args) {
+		System.out.println("First arg : " + args[0]);
+		System.out.println("Second arg: " + args[1]);
+	}
+}
